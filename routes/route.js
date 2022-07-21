f33cb905ed749d11ca8a847eef6f4ab6e278aa73
ayusshrathore <ayusshrathore@gmail.com>
2022-07-21 17:25:31 +0530

    Add routes for api

---
+++ routes/route.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const router = express();
+
+const adminController = require("../controller/adminController");
+const userController = require("../controller/userController");
+const productController = require("../controller/productController");
+const blogController = require("../controller/blogController");
+const customerController = require("../controller/customerController");
+const cartController = require("../controller/cartController");
+const orderController = require("../controller/orderController");
+const paymentController = require("../controller/paymentController");
+
+const mail = require("../helper/mail");
+const upload = require("../helper/storage");
+const checkAuth = require("../middleware/checkAuth");
+
+// Home route
+router.get("/", (req, res) => {
+	res.send("Server is up and running...");
+});
+
+// Admin routes
+router.post("/api/v1/admin/register", adminController.register);
+router.post("/api/v1/admin/login", adminController.login);
+router.post("/api/v1/admin/delete", checkAuth, adminController.delete);
+router.get("/api/v1/admin/get", checkAuth, adminController.get);
+
+// User routes
+router.post("/api/v1/user/register", userController.register);
+router.post("/api/v1/user/login", userController.login);
+router.post("/api/v1/user/update", checkAuth, userController.update);
+router.post(
+	"/api/v1/user/update/address",
+	checkAuth,
+	userController.updateAddress
+);
+router.post("/api/v1/user/delete", checkAuth, userController.delete);
+router.post("/api/v1/user/get", checkAuth, userController.getUser);
+router.get("/api/v1/user/getAll", checkAuth, userController.get);
+
+// Product routes
+router.post(
+	"/api/v1/product/add",
+	checkAuth,
+	upload.single("image"),
+	productController.add
+);
+router.post(
+	"/api/v1/product/update",
+	checkAuth,
+	upload.single("image"),
+	productController.update
+);
+router.post("/api/v1/product/delete", checkAuth, productController.delete);
+router.get("/api/v1/product/get", productController.get);
+
+// Blog routes
+router.post(
+	"/api/v1/blog/add",
+	checkAuth,
+	upload.single("image"),
+	blogController.add
+);
+router.post(
+	"/api/v1/blog/update",
+	checkAuth,
+	upload.single("image"),
+	blogController.update
+);
+router.post("/api/v1/blog/delete", checkAuth, blogController.delete);
+router.get("/api/v1/blog/get", blogController.get);
+
+// Customer routes
+router.post("/api/v1/customer/add", customerController.add);
+router.get("/api/v1/customer/get", customerController.get);
+
+// Mail routes
+router.post("/api/v1/mail/send", mail.send);
+
+// Cart routes
+router.post("/api/v1/cart/add", checkAuth, cartController.add);
+router.post("/api/v1/cart/update", checkAuth, cartController.update);
+router.post("/api/v1/cart/delete", checkAuth, cartController.delete);
+router.post("/api/v1/cart/get", checkAuth, cartController.get);
+router.get("/api/v1/cart/getAll", checkAuth, cartController.getAll);
+
+// Order routes
+router.post("/api/v1/order/create", checkAuth, orderController.create);
+router.post("/api/v1/order/cancel", checkAuth, orderController.cancel);
+router.post("/api/v1/order/get", checkAuth, orderController.get);
+
+// Payment routes
+router.post("/api/v1/payment/create", checkAuth, paymentController.create);
+router.post("/api/v1/payment/verify", checkAuth, paymentController.verify);
+
+// Check authentication
+router.post("/api/v1/auth", checkAuth, (req, res) => {
+	res.send({
+		message: "Authentication successful!",
+	});
+});
+
+module.exports = router;
